7118e1dc4dd363ac1eb30e01b4cf7775230b96b8
Eric Mills <emills@users.noreply.github.com>
2014-07-18 00:00:11 -0400

    Add additional documentation

--- lib/wiki_auto_increment_macros.rb
+++ lib/wiki_auto_increment_macros.rb
@@ -49,25 +49,37 @@ module WikiAutoIncrement
     end
 
     Redmine::WikiFormatting::Macros.register do
-      desc ""
+      desc "Increments a number with the first call returning '1' Examples:\n\n" +
+      "  !{{auto_increment}} -- Will return 1 on first use, 2 on second use, etc." +
+      "  !{{auto_increment(foobar)}} -- Will associate the incremented value with the name 'foobar'. Allows for multiple increments on the same page." +
+      "  !{{auto_increment(operation)}} -- Performs specified operation against current value. (+ => Add One, - => Subtract One, = => Current Value, ! => Reset To 1)" +
+      "  !{{auto_increment(foobar, operation)}} -- Combination of previous two examples."
       macro :auto_increment do |obj, args|
         @values ||= {}
         WikiAutoIncrement::Macros.doAutoIncrement(@values, args)
       end
-      
-      desc ""
+
+      desc "Increment the lower case alphabet with the first call returning 'a'. When 'z' is reached the counter will wrap to 'a'." +
+      "  !{{auto_increment_lower}} -- Will return a on first use, b on second use, etc." +
+      "  !{{auto_increment_lower(foobar)}} -- Will associate the incremented value with the name 'foobar'. Allows for multiple increments on the same page." +
+      "  !{{auto_increment_lower(operation)}} -- Performs specified operation against current value. (+ => Add One, - => Subtract One, = => Current Value, ! => Reset To 1)" +
+      "  !{{auto_increment_lower(foobar, operation)}} -- Combination of previous two examples."
       macro :auto_increment_lower do |obj, args|
         @values_alpha_lower ||= {}
         WikiAutoIncrement::Macros.doAlphaAutoIncrement(ALPHA_LOWER, @values_alpha_lower, args)
       end
-      
-      desc "Alias of \{{auto_increment_alpha_lower}}"
+
+      desc "Alias of !{{auto_increment_alpha_lower}}"
       macro :auto_increment_alpha do |obj, args|
         @values_alpha_lower ||= {}
         WikiAutoIncrement::Macros.doAlphaAutoIncrement(ALPHA_LOWER, @values_alpha_lower, args)
       end
 
-      desc ""
+      desc "Increment the upper case alphabet with the first call returning 'A'. When 'Z' is reached the counter will wrap to 'A'." + 
+      "  !{{auto_increment_upper}} -- Will return A on first use, B on second use, etc." +
+      "  !{{auto_increment_upper(foobar)}} -- Will associate the incremented value with the name 'foobar'. Allows for multiple increments on the same page." +
+      "  !{{auto_increment_upper(operation)}} -- Performs specified operation against current value. (+ => Add One, - => Subtract One, = => Current Value, ! => Reset To 1)" +
+      "  !{{auto_increment_upper(foobar, operation)}} -- Combination of previous two examples."
       macro :auto_increment_upper do |obj, args|
         @values_alpha_upper ||= {}
         WikiAutoIncrement::Macros.doAlphaAutoIncrement(ALPHA_UPPER, @values_alpha_upper, args)
